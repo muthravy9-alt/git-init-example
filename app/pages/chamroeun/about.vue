59a23a7f73776c5bd161f3d54646e9848cb937bf
Cham Roeun <tunchamroeun@cloudware.com.kh>
2026-01-30 13:16:52 +0700

    add about page

---
+++ app/pages/chamroeun/about.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    email: tunchamroeun@cloudware.com.kh <br>
+    tel: 096 4962424
+  </div>
+</template>
--- app/pages/chamroeun/index.vue
+++ app/pages/chamroeun/index.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
     Hello this is Cham Roeun Page
+    <UButton to="/chamroeun/about">
+      About me
+    </UButton>
   </div>
 </template>
